df35c480839bc316c77fca7bfb9a125d797f4c17
Ahmedbt1 <ahmedbt1@aol.com>
2021-11-11 13:25:45 +0000

    updated public key

--- key-pair/key-pair.tf
+++ key-pair/key-pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "ilearning-wordpress" {
   key_name   = "ilearning-wordpress"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD2jsY8NpOQf/2x65RO9wAK6iBwmg8RrGa20IjwBKG/u9kpGeuD5BA7NsWHjLBIzzC9TvJjtl8BqesCSwIM5cUnUvnUTr/rF6MuWIuFKFYtupND5wWMhoZ5lJ5LW73+WOsWakEZ6yTueJIySlSRfEES3nbDg3c298IJadA3Er4Dat3T/JF7i29IHgI29Ls5Q5m+apcOxZ+4V4FUiyXjM3y0Jsi4pSRAgeEVWYmKFWfq7e3lK4WyoyU+3CMsBv0z2unXFMXmArT7CzmrKi+MTZiOt6eAYUD3dN4VJa64Qe5+7/S7LuRQ/rQD3mlOgNo46J2vPI1xhvhIsg3FZlQJsZT9 ec2-user@ip-172-31-5-128.eu-west-1.compute.internal"
 }
