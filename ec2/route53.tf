3bdcf65535aa6ebabc10cfba6cc497732690bf96
Ahmedbt1 <ahmedbt1@aol.com>
2021-11-12 22:36:39 +0000

    changed domain name

--- ec2/route53.tf
+++ ec2/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "wordpress" {
   zone_id = "Z05584593KK6MIYIUNKLA"
-  name    = "wordpress.benjelloun.click"
+  name    = "wordpress.yourdomain"
   type    = "A"
   ttl     = "30"
   records = [aws_instance.web.public_ip]
