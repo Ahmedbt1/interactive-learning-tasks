4efe1c999368ea9c6a03c017c4611fc82a6d2501
Ahmedbt1 <ahmedbt1@aol.com>
2021-11-12 23:00:51 +0000

    added gitignore to sec_group folder

--- ec2/route53.tf
+++ ec2/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "wordpress" {
   zone_id = "Z05584593KK6MIYIUNKLA"
-  name    = "wordpress.yourdomain"
+  name    = "wordpress.benjelloun.click"
   type    = "A"
   ttl     = "30"
   records = [aws_instance.web.public_ip]
